4592a3db3e866eb0cd111ca29bb86e99a2fde8d9
Prathamesh Kulkarni <prathameshkulkarni47@gmail.com>
2024-06-02 22:07:22 +0530

    feat: Implement appointment-service
    * Implemented appointment-service

--- src/main/java/org/dnyanyog/AppointmentServiceMain.java
+++ src/main/java/org/dnyanyog/AppointmentServiceMain.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayMain {
-
-  public static void main(String[] args) {
-    SpringApplication.run(ApiGatewayMain.class, args);
+public class AppointmentServiceMain {
+  public static void main(String args[]) {
+    SpringApplication.run(AppointmentServiceMain.class, args);
   }
 }
